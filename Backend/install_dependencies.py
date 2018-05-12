034800b2ce588a612580069bb9144b4d6a923402
udith <udithprabhu@gmail.com>
2018-05-12 23:27:43 +0530

    install dependencies added

--- Backend/install_dependencies.py
+++ Backend/install_dependencies.py
@@ -1,1 +1,14 @@
 
+import os
+
+cmds = [
+	"npm i -g electron --unsafe-perm=true --allow-root" ,
+	"npm i -g http" ,
+	"npm i -g net" ,
+	"npm i -g electron-ipc" , 
+	"npm i -g system-sleep" , 
+	"sudo apt-get install wmctrl"
+]
+
+for cmd in cmds :
+	os.system(cmd)
